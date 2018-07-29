7944a39070bbb81ed261cfbd51d17b79097ed762
Gyanesh Gouraw <gyaneshgouraw@gmail.com>
2018-07-29 09:16:13 +0530

    server port changed

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000,()=>{console.log('Server started @ port 9000')});
+app.listen(3000);
