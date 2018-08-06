c196a14fcdb741786927da3ea34bb7890fd69869
Gyanesh Gouraw <gyaneshgouraw@gmail.com>
2018-08-06 09:59:01 +0530

    basic rectification

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const validator = require('express-validator');
 const path = require('path');
+var passport = require('passport'),
+GoogleOAuth2Strategy = require('passport-google-auth').Strategy;
 
 
 
@@ -31,20 +33,53 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression());
 app.use(validator());
 
+/**
+ * Passport auth
+ */
+let user = {}
+passport.use(new GoogleOAuth2Strategy({
+    clientId: '651347370973-gjqgapp9jlpgj7eol94gku9rvvdr30p3.apps.googleusercontent.com',
+    clientSecret: 'OmsoiNTQcfHHpRxt4XY4-s56',
+    callbackURL: 'http://localhost:8080/auth/callback/google'
+  },
+  function(accessToken, refreshToken, profile, callback) {
+    user=profile
+    return  callback()
+  }
+));
+
+// app.get('/loging', passport.authenticate('google'))
+app.get('/loging', passport.authenticate('google', { scope: [
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read'] 
+}));
+
+// app.get('/auth/callback/google', 
+//    // passport.authenticate('google', {successRedirect:'/q', failureRedirect: '/hello' ,failureFlash:true}),
+//    passport.authenticate('google'), 
+//    function(req, res) {
+//         // Successful authentication, redirect to your app.
+//         console.log(req)
+//         res.redirect('/child1');
+//     }
+// );
+
+app.get('/auth/callback/google',(req, res) => res.redirect('/'))
+
 
 // Initialise the API routes
 app.use('/api', require('./routes/scheduler.routes'));
-app.get('/api/ping', (req, res) => res.send('pong'));
+app.get('/api/ping', (req, res) => res.json({ a: 1 }));
 
 
 
 //serving client
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 
 //Error page
-app.use((req, res) => res.status(404).json({code: 404, message: 'HTTP 404 Not Found'}));
+//app.use((req, res) => res.status(404).json({code: 404, message: 'HTTP 404 Not Found'}));
 
 
 
